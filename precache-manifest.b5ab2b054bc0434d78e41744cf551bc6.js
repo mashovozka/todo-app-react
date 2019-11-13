ea9618a8d0e5c95a435c0fc8c408ad004c2a0f25
mashovozka <mashovozka@gmail.com>
2019-11-13 14:04:14 +0100

    Updates

---
+++ precache-manifest.b5ab2b054bc0434d78e41744cf551bc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad79aad947a005996aeea07101679032",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "17d4bf70373c200143d2",
+    "url": "/todo-app-react/static/css/main.5c49bca6.chunk.css"
+  },
+  {
+    "revision": "40be87a76af1e5eb86bd",
+    "url": "/todo-app-react/static/js/2.9987a9c2.chunk.js"
+  },
+  {
+    "revision": "17d4bf70373c200143d2",
+    "url": "/todo-app-react/static/js/main.26ac5887.chunk.js"
+  },
+  {
+    "revision": "5389d73cace928ddaa32",
+    "url": "/todo-app-react/static/js/runtime-main.6d39e119.js"
+  }
+]);
